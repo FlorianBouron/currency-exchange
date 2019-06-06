800cbbac954cfb8c072b0037b7bf365f643a54f4
Florian Bouron <bouron.florian@yahoo.fr>
2019-06-07 00:16:40 +0200

    Add unit tests for rates selectors

---
+++ src/redux/selectors/rates.spec.js
@@ -0,0 +1,19 @@
+import store from "../store.mock";
+import { getRates, getRateByName, getRateBase, getRateError } from "./rates";
+
+test("selector getRates return the expected result", () => {
+  expect(getRates(store)).toBe(store.rates.rates);
+});
+
+test("selector getRateByName return the expected result", () => {
+  const currency = "USD";
+  expect(getRateByName(store, currency)).toBe(store.rates.rates[currency]);
+});
+
+test("selector getRateBase return the expected result", () => {
+  expect(getRateBase(store)).toBe(store.rates.base);
+});
+
+test("selector getRateError return the expected result", () => {
+  expect(getRateError(store)).toBe(store.rates.error);
+});
