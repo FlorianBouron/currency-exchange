0a6793ebebdb06ec555f3bccceed14a4d64e16d1
Florian Bouron <bouron.florian@yahoo.fr>
2019-06-07 17:38:27 +0200

    Truncate rate during display for better UX

--- src/redux/selectors/rates.js
+++ src/redux/selectors/rates.js
@@ -1,5 +1,5 @@
 export const getRates = state => state.rates.rates;
 export const getRateByName = (state, name) =>
-  state.rates.rates ? state.rates.rates[name] : null;
+  state.rates.rates ? state.rates.rates[name].toFixed(3) : null;
 export const getRateBase = state => state.rates.base;
 export const getRateError = state => state.rates.error;
